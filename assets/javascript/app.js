7a821888acd2d188c8ad67e84f225baee7ed2858
Rebecca Bartl <rebecca.bartl@gmail.com>
2019-09-15 13:41:06 -0500

    added stats call and table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,78 @@
 $(document).ready(function(){
 
+function getStats(){
+    $('.stats-container').empty();
+    var userInput = $('#user-input').val().trim();
+    userInput = userInput.split(" ").join("-").toLowerCase();
+    console.log(userInput);
+    var queryURL = "https://api.mysportsfeeds.com/v1.1/pull/nfl/2019-regular/cumulative_player_stats.json?&player=" + userInput;
+    $.ajax({
+      url: queryURL,
+      type: "GET",
+      dataType: "json",
+      headers: { 
+        "Authorization": "Basic " + btoa("a437f7dc-b82f-4f70-8989-798c15" + ":" + "Classwork12")
+     }
+    }).then(function(response){
+      console.log(response);
+      var table = $('<table>');
+      var thead = $('<thead>');
+      var tbody = $('<tbody>');
+      var tableDiv = $('<div>');
+      tableDiv.addClass('table-container');
+      table.addClass('table');
+      thead.addClass('thead');
+      tbody.addClass('tbody');
+      var name = response.cumulativeplayerstats.playerstatsentry[0].player.FirstName + " " + response.cumulativeplayerstats.playerstatsentry[0].player.LastName;
+      var position = response.cumulativeplayerstats.playerstatsentry[0].player.Position;
+      var team = response.cumulativeplayerstats.playerstatsentry[0].team.City + " " + response.cumulativeplayerstats.playerstatsentry[0].team.Name;
+      var h1Name = $('<h1>');
+      h1Name.addClass('is-size-3');
+      h1Name.text(name);
+      var pPosition = $('<p>');
+      pPosition.text(position);
+      var pTeam = $('<p>');
+      pTeam.addClass('is-italic');
+      pTeam.text(team);
+      $('.stats-container').append(h1Name, pTeam, pPosition);
+      var h2Passing = $('<h2>');
+      h2Passing.addClass('is-size-4');
+      h2Passing.text('Passing');
+      $('.stats-container').append(h2Passing);
+      $('.stats-container').append(table);
+      $('.table').append(thead);
+      $('.table').append(tbody);
+      var header = $('<tr>').append(
+        $('<th>').text('ATT'),
+        $('<th>').text('CMP'),
+        $('<th>').text('CMP%'),
+        $('<th>').text('YARDS'),
+        $('<th>').text('AVG'),
+        $('<th>').text('INT'),
+        $('<th>').text('TD'),
+        $('<th>').text('FUM'),
+        $('<th>').text('SACK'),
+        );
+        $('.thead').append(header);
+      var newRow = $('<tr>').append(
+        $('<td>').text(response.cumulativeplayerstats.playerstatsentry[0].player.stats.PassAttempts.text),
+        $('<td>').text(response.cumulativeplayerstats.playerstatsentry[0].player.stats.PassCompletions.text),
+        $('<td>').text(response.cumulativeplayerstats.playerstatsentry[0].player.stats.PassPct.text),
+        $('<td>').text('YARDS'),
+        $('<td>').text('AVG'),
+        $('<td>').text('INT'),
+        $('<td>').text('TD'),
+        $('<td>').text('FUM'),
+        $('<td>').text('SACK'),
+        );
+      $('.tbody').append(newRow);
+
+
+
+
+    });
+  };
+
 function getNews(){
     var queryURL = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/news"
     $.ajax({
@@ -30,7 +103,6 @@ function getNews(){
             content.append(br);
             var aElement = $('<a>');
             var link = response.articles[i].links.web.href;
-            console.log(link);
             aElement.attr('href', link);
             aElement.attr('target', '_blank');
             aElement.text(' read more');
@@ -43,6 +115,7 @@ function getNews(){
     });
 };
 getNews();
+$(document).on('click', '#submit', getStats);
 
 
 
